40d234177e9bd86559649251358c3c0b619eae41
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-08-29 23:41:20 +0500

    Added project`s dictionary of terms

--- informaticssoft/mainmenu/urls.py
+++ informaticssoft/mainmenu/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import mainmenu_view
+from .views import mainmenu_view, FAQ_terms_view
 
 
 urlpatterns = [
-    path('', mainmenu_view)
+    path('', mainmenu_view),
+    path('FAQ/terms', FAQ_terms_view)
 ]
--- informaticssoft/mainmenu/views.py
+++ informaticssoft/mainmenu/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 
 def mainmenu_view(request):
     return render(request, template_name='start_menu.html')
+
+
+def FAQ_terms_view(request):
+    # Словарь проекта
+    with open(f'{settings.BASE_DIR}\\media\\txt_content\\project_terms.txt', 'r', encoding='utf-8') as file:
+        content = file.read().split('\n')
+        content = '<br>'.join(content)
+    return HttpResponse('<title>Словарь проекта</title> <b>Словарь терминов проекта:</b><br><br><br>{}'.format(content))
