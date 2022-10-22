a186468f2533a1b585057ea5115f2f5836484b80
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-10-22 19:52:01 +0500

    create tests for static urls

--- informaticssoft/mainmenu/tests.py
+++ informaticssoft/mainmenu/tests.py
@@ -1,3 +1,27 @@
-# from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class StaticUrlTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_mainmenu(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_faqpage(self):
+        response = self.client.get('/FAQ/terms/')
+        self.assertEqual(
+            response.status_code,
+            200,
+            'Верный адрес не вернул код 200: {}'.format(response.status_code))
+        response = self.client.get('/FAQ/Test/')
+        self.assertEqual(
+            response.status_code,
+            404,
+            'Неверный адрес не вернул код 404')
+        response = self.client.get('/FAQ/')
+        self.assertEqual(
+            response.status_code,
+            404,
+            'Неверный адрес не вернул код 404')
--- informaticssoft/py_compiler/tests.py
+++ informaticssoft/py_compiler/tests.py
@@ -1,3 +1,60 @@
-# from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class StaticUrlTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_code_editor_page(self):
+        response = self.client.get('/code-editor/')
+        self.assertEqual(
+            response.status_code,
+            200,
+            'Главная страница редактора выдает код: {}'.format(
+                response.status_code
+            )
+        )
+
+    def test_code_input_page(self):
+        response = self.client.get('/code-editor/input/')
+        self.assertEqual(
+            response.status_code,
+            301,
+            'Редирект на страницу редактора не произошел'
+        )
+        session = self.client.session
+        session['inputs_lines'] = [
+            {
+                'code_line': 'a = input()\r',
+                'line_index': 0
+            }
+            ]
+        session.save()
+        response = self.client.get('/code-editor/input/')
+        self.assertEqual(
+            response.status_code,
+            200,
+            'Страница с вводом данных вернула код: {}'.format(
+                response.status_code
+            )
+        )
+
+    def test_manual_page(self):
+        response = self.client.get('/manual/')
+        self.assertEqual(
+            response.status_code,
+            200,
+            'Страница с мануалом не открылась. Код: {}'.format(
+                response.status_code
+                )
+        )
+
+    def test_contacts_page(self):
+        response = self.client.get('/contacts/')
+        self.assertEqual(
+            response.status_code,
+            200,
+            'Страница для обратной связи не открылась. Код: {}'.format(
+                response.status_code
+            )
+        )
