5ea8b5b8ca613dd59ee7060c378d1f0bfdd72a3f
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-10-22 19:54:16 +0500

    make dynamic url

--- informaticssoft/mainmenu/views.py
+++ informaticssoft/mainmenu/views.py
@@ -7,18 +7,21 @@ def mainmenu_view(request):
     return render(request, template_name='start_menu.html')
 
 
-def FAQ_terms_view(request):
-    # Словарь проекта
-    with open(
-        f'{settings.BASE_DIR}\\media\\txt_content\\project_terms.txt',
-        'r',
-        encoding='utf-8'
-            ) as file:
-        content = file.read().split('\n')
-        content = '<br>'.join(content)
-    return HttpResponse(
-        (
-            '<title>Словарь проекта</title> '
-            '<b>Словарь терминов проекта:</b><br><br><br>{}')
-        .format(content)
-        )
+def FAQ_view(request, faq_type):
+    if faq_type == 'terms':
+        # Словарь проекта
+        with open(
+            f'{settings.BASE_DIR}\\media\\txt_content\\project_terms.txt',
+            'r',
+            encoding='utf-8'
+                ) as file:
+            content = file.read().split('\n')
+            content = '<br>'.join(content)
+        return HttpResponse(
+            (
+                '<title>Словарь проекта</title> '
+                '<b>Словарь терминов проекта:</b><br><br><br>{}')
+            .format(content)
+            )
+    else:
+        return HttpResponse(status=404)
