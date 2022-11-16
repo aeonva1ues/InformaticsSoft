396b1d21878c2d030946122b0fd50c9566a9b75c
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-11-16 18:09:19 +0500

    add url for examples page

--- informaticssoft/py_compiler/urls.py
+++ informaticssoft/py_compiler/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import contacts_page, manual_page, py_compiler_view, user_inputs
+from .views import (
+    contacts_page, manual_page,
+    py_compiler_view, user_inputs, examples_page
+    )
 
 urlpatterns = [
     path('code-editor/', py_compiler_view),
     path('code-editor/input/', user_inputs),
     path('contacts/', contacts_page),
-    path('manual/', manual_page)
+    path('manual/', manual_page),
+    path('examples/', examples_page)
 ]
