cd303223ba8c915cac7c2afc3c6fd8caf50bfef1
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-11-16 22:14:45 +0500

    add new table videoexample

--- informaticssoft/py_compiler/admin.py
+++ informaticssoft/py_compiler/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from py_compiler.models import Manual_Section, Manual_Infoblock
+from py_compiler.models import (
+    Manual_Section, Manual_Infoblock,
+    VideoExample
+    )
 
 
 @admin.register(Manual_Section)
@@ -16,3 +19,14 @@ class InfoblockAdmin(admin.ModelAdmin):
     list_display_links = ('block_name',)
     list_editable = ('is_published',)
     list_filter = ('section__name',)
+
+
+@admin.register(VideoExample)
+class VideoContentAdmin(admin.ModelAdmin):
+    list_display = ('get_lesson_name', 'link', 'is_published')
+    list_editable = ('is_published',)
+    # list_filter = ('get_lesson_name',)
+
+    def get_lesson_name(self, obj):
+        return obj.lesson.block_name
+    get_lesson_name.short_description = 'тема'
--- informaticssoft/py_compiler/models.py
+++ informaticssoft/py_compiler/models.py
@@ -30,3 +30,35 @@ class Manual_Infoblock(models.Model):
     class Meta:
         verbose_name = 'статья'
         verbose_name_plural = 'статьи'
+
+
+class VideoManager(models.Manager):
+    def published(self):
+        return (
+            self.get_queryset()
+            .select_related('lesson')
+            .filter(is_published=True, lesson__is_published=True)
+        )
+
+
+class VideoExample(models.Model):
+    lesson = models.ForeignKey(
+        Manual_Infoblock,
+        verbose_name='тема',
+        on_delete=models.CASCADE,
+        related_name='videoexample'
+    )
+    link = models.URLField(
+        'ссылка на видео',
+        max_length=150
+    )
+    is_published = models.BooleanField('отображать', default=True)
+
+    def __str__(self):
+        return self.link
+
+    class Meta:
+        verbose_name = 'видеоролик'
+        verbose_name_plural = 'видеоролики'
+
+    objects = VideoManager()
