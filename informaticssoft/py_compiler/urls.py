3461df08fe54f9dd41c556098dfbb055d38d928d
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-11-19 20:52:46 +0500

    remove extra view controller

--- informaticssoft/py_compiler/urls.py
+++ informaticssoft/py_compiler/urls.py
@@ -3,16 +3,13 @@ from django.urls import path
 from .views import (
     contacts_page, manual_page,
     py_compiler_view, user_inputs, examples_page,
-    get_video
     )
 
 
-app_name = 'pytpr'
 urlpatterns = [
     path('code-editor/', py_compiler_view),
     path('code-editor/input/', user_inputs),
     path('contacts/', contacts_page),
     path('manual/', manual_page),
     path('examples/', examples_page),
-    path('examples/get_video', get_video, name='get-video')
 ]
